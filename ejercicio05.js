3b1e767bf5e97f2ff7307a160e8eecc11878280d
[caebar-backend] <[caebar.cursos.programacion@gmail.com]>
2025-04-07 21:00:17 -0300

    ejercicio05.js completado

--- ejercicio05.js
+++ ejercicio05.js
@@ -1,3 +1,22 @@
 // Ejercicio 5
-// Imprimir los números pares desde N (siendo N un número par que se lee) en forma descendente
+// Imprimir los números pares desde N (siendo N un número par que se lee) en forma descendente ?
 // hasta 2.
+const chalk = require('chalk')
+const prompt = require('prompt-sync')
+const ps = prompt()
+
+let Narray = []
+let doWhileVar = 1
+const N = parseInt(ps('Escriba un número desde 1 hasta 1000: '))
+
+do {
+doWhileVar % 2 === 0 ? Narray.push(doWhileVar) : ''
+
+doWhileVar++
+}
+while ( doWhileVar <= N)
+
+const NarrayReverse = Narray.reverse()
+console.log(chalk.yellow(`${NarrayReverse}`))
+
+
