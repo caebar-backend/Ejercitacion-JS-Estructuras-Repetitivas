2758670e3e9de9d558e5ae329419133a34641b45
[caebar-backend] <[caebar.cursos.programacion@gmail.com]>
2025-04-13 02:52:16 -0300

    ejercicio11.js completado

--- ejercicio11.js
+++ ejercicio11.js
@@ -1,1 +1,28 @@
 // 11. Calcular el factorial de 10 números diferentes cuyos valores se leen.
+
+
+const prompt = require('prompt-sync')
+const ps = prompt()
+const chalk = require('chalk')
+
+let i = 0
+
+function factorialRecursivo(n) {
+    if (n === 0) {
+      return 1; // Caso base: 0! = 1
+    } else {
+      return n * factorialRecursivo(n - 1); // Llamada recursiva
+    }
+  }
+
+do{
+const nuM = parseInt(ps(`Escribe un número para calcular el factorial: `))
+console.log(factorialRecursivo(nuM));
+i++
+if(i === 9){
+    break;
+}
+
+}while(true )
+
+console.log(chalk.bgBlueBright('Se cierra la aplicación!!!              '))
