695ecca2c4b2315ec8ebe6cd24a9c14fad68c7dd
[caebar-backend] <[caebar.cursos.programacion@gmail.com]>
2025-04-13 02:50:27 -0300

    ejercicio09.js

--- ejercicio09.js
+++ ejercicio09.js
@@ -1,2 +1,24 @@
 // Ejercicio 9
-// Imprimir los números de 1 a N (siendo N un número que se lee) cada uno con su respectivo factorial.
+// Imprimir los números de 1 a N (siendo N un número que se lee) cada uno con su respectivo factorial. 
+
+const prompt = require('prompt-sync')
+const ps = prompt()
+
+function calcularFactorial(num) {
+    let factorial = 1;
+    for (let i = 1; i <= num; i++) {
+        factorial *= i;
+    }
+    return factorial;
+}
+
+const N = parseInt(ps("Ingrese un número N:   ->>  "));
+
+if (isNaN(N) || N < 1) {
+    console.log("Por favor ingrese un número válido mayor que 0");
+} else {
+    console.log(`Números del 1 al ${N} con sus factoriales:`);
+    for (let i = 1; i <= N; i++) {
+        console.log(`${i}! = ${calcularFactorial(i)}`);
+    }
+}
