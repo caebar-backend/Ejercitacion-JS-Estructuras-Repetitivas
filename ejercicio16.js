313f6139c63290f9bfd4471eef65b0dd1664f57a
[caebar-backend] <[caebar.cursos.programacion@gmail.com]>
2025-04-13 02:54:35 -0300

    ejercicio16.js culminado

--- ejercicio16.js
+++ ejercicio16.js
@@ -1,3 +1,26 @@
 // 16. En 1994 el país A tiene una población de 25 millones de habitantes y el país B de 19.9 millones. Las
 // tasas de crecimiento de la población son de 2% y 3% respectivamente. Desarrollar un algoritmo para
 // informar en que año la población del país B supera a la de A.
+
+
+
+const tasaCrecimientoPaisA = 2
+const tasaCrecimientoPaisB = 3
+
+let contador = 1994
+let paisA = 25000000
+let paisB = 19900000
+
+
+do{
+
+contador++
+const crecimientoPaisA = (paisA * 2) / 100
+const crecimientoPaisB = (paisB * 3) / 100
+paisA = paisA + crecimientoPaisA
+paisB = paisB + crecimientoPaisB
+
+}while (paisA > paisB)
+
+console.log('En el año ' + contador + ' la población del país B supera a la del país A')
+
